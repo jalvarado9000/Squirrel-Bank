0fd6e4d22e3e5edef976641c3c1d53396498dca2
jalvarado9000 <jalvarado9000@gmail.com>
2024-03-23 14:37:56 -0700

    small update

--- src/Balance.jsx
+++ src/Balance.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-const Balance = ({balance}) => {
+const Balance = ({ balance }) => {
 
 
-  let current = balance.reduce((counter, value) =>{
+  let current = balance.reduce((counter, value) => {
     return counter + value.amount;
-  },0);
+  }, 0);
 
   return (
-      <>
+    <>
       <div className="balance">
-          <div>Balance</div>
-          <span id="currency">$</span>
-          <span id="balance">{current}</span>
-          
+        <div>Balance</div>
+        <span id="currency">$</span>
+        <span id="balance">{current}</span>
+
       </div>
-      </>
+    </>
   )
 }
 export default Balance;

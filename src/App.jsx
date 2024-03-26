e11f0c0329ad4b73e2556ba1addf3cfc27af1921
jalvarado9000 <jalvarado9000@gmail.com>
2024-03-25 18:38:56 -0700

    make changes to use BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-
 import './App.css'
-
 import transactions from './TRANSACTIONS'
-
 import { LoginPage } from './LoginPage'
 import { RegisterPage } from './RegisterPage'
 import { DashboardPage } from './DashboardPage'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 
 
 function App() {
@@ -23,25 +21,36 @@ function App() {
 
   return (
     <>
+
+      <BrowserRouter>
       <div className="dashboard-page">
  
 
-        <LoginPage />
+          <nav>
+            
+            <NavLink to="/">Register</NavLink>
+            <br />
+            <NavLink to="Dashboard">Dashboard</NavLink>
+            <br />
+            <NavLink to="Login">Login</NavLink>
+          </nav>
 
-        <DashboardPage 
-        balance={allTransaction} 
-        title={accountNumber} 
-        history={allTransaction} 
-        titles={addTransactionLabel} 
-        newTransaction={handleNewTransactions} 
-        />
+          <Routes>
+            <Route index element={<RegisterPage />} />
+            <Route path="Dashboard" element={<DashboardPage balance={allTransaction}
+              title={accountNumber}
+              history={allTransaction}
+              titles={addTransactionLabel}
+              newTransaction={handleNewTransactions} />} />
+            <Route path="Login" element={<LoginPage />} />
+          </Routes>
         
-        <RegisterPage />
         
        
 
 
       </div>
+      </BrowserRouter>
 
     </>
   )
